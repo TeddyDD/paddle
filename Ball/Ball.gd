d6d4763365834f358ba586083e8a2715a2c94aec
Kamil Lewan <carmel4a@o2.pl>
2017-04-10 15:31:07 +0200

    Working bounce ball

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -8,11 +8,41 @@ var green = preload("res://Ball/ball_green.png")
 var red = preload("res://Ball/ball_red.png")
 var grey = preload("res://Ball/ball_grey.png")
 
+var dir = Vector2(1,1)
+var ppos = Vector2(0,0)
+var speed = 50
 var _ready = false
 
 func _ready():
 	_ready = true
 	update_all()
+	set_process(true)
+	
+func _process(delta):
+	ppos = get_pos()
+	if (Vector2(ppos + Vector2(dir * speed * Vector2(delta,delta))).x+(get_node(size).get_region_rect().size.x/2)<get_viewport_rect().size.x):
+		set_pos(ppos + Vector2(dir * speed * Vector2(delta,delta)))
+	else:
+		dir.x = -dir.x
+		set_pos(ppos + Vector2(dir * speed * Vector2(delta,delta)))
+	if (Vector2(ppos + Vector2(dir * speed * Vector2(delta,delta))).x-(get_node(size).get_region_rect().size.x/2)>0):
+		set_pos(ppos + Vector2(dir * speed * Vector2(delta,delta)))
+	else:
+		dir.x = -dir.x
+		set_pos(ppos + Vector2(dir * speed * Vector2(delta,delta)))
+	if (Vector2(ppos + Vector2(dir * speed * Vector2(delta,delta))).y+(get_node(size).get_region_rect().size.y/2)<get_viewport_rect().size.y):
+		set_pos(ppos + Vector2(dir * speed * Vector2(delta,delta)))
+	else:
+		dir.y = -dir.y
+		set_pos(ppos + Vector2(dir * speed * Vector2(delta,delta)))
+	if (Vector2(ppos + Vector2(dir * speed * Vector2(delta,delta))).y-(get_node(size).get_region_rect().size.y/2)>0):
+		set_pos(ppos + Vector2(dir * speed * Vector2(delta,delta)))
+	else:
+		dir.y = -dir.y
+		set_pos(ppos + Vector2(dir * speed * Vector2(delta,delta)))
+	
+	
+	
 	
 func setcolor(value):
 	color = value
@@ -26,7 +56,7 @@ func setsize(value):
 func update_all():
 	for c in get_children():
 		if c == null:
-			continue
+			break
 		if c.get_type() == "Sprite": 
 			if c.get_name() != size:
 				c.get_node("Area2D").set_enable_monitoring(false)
