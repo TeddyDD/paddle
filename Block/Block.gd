7d21ee9bfff0ddd8539ab117cad3b90436ed0e06
Daniel Lewan <vision360.daniel@gmail.com>
2017-04-11 17:54:28 +0200

    add random blocks for testing implemented blocs look change when hit

--- Block/Block.gd
+++ Block/Block.gd
@@ -3,15 +3,18 @@ extends Area2D
 
 export(int, "easy1", "easy2", "medium1", "medium2", "hard") var type = 0 setget set_type
 var life = 1
+var layer
+onready var sprite = get_node("sprite")
 
 var _anim_names = ["easy1", "easy2", "medium1", "medium2", "hard"]
 
 func _ready():
 	_set_life(type)
+	sprite.set_animation(_anim_names[type])
 
 func set_type(value):
-	if get_node("sprite") != null:
-		get_node("sprite").set_animation(_anim_names[value])
+	if sprite != null:
+		sprite.set_animation(_anim_names[value])
 	type = value
 	_set_life( type )
 
@@ -33,3 +36,5 @@ func _on_Block_area_enter( area ):
 		life -= 1
 		if life <=0:
 			queue_free()
+		else: 
+			sprite.set_frame(sprite.get_frame() + 1)
--- Game.gd
+++ Game.gd
@@ -14,4 +14,6 @@ func _ready():
 func set_block(x,y):
 	blocks[x][y] = load("res://Block/Block.tscn").instance()
 	blocks[x][y].set_pos(Vector2(x*16,y*8+3*16))
+	blocks[x][y].layer = y
+	blocks[x][y].type = randi() % 5
 	add_child(blocks[x][y])
