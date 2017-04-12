a4ed8b82e2c7d3fa6596cd28207b0d2540afe80b
Daniel Lewan <vision360.daniel@gmail.com>
2017-04-12 19:55:06 +0200

    add lot of new people to smash with ball :D

---
+++ pico8/pico_colors.gd
@@ -0,0 +1,19 @@
+extends Reference
+
+# color names form http://www.romanzolotarev.com/pico-8-color-palette/
+var black       = Color(0, 0, 0)
+var dark_blue   = Color(29, 43, 83)
+var dark_purple = Color(126, 37, 83)
+var dark_green  = Color(0, 135, 81)
+var brown       = Color(171, 82, 54)
+var dark_gray   = Color(95, 87, 79)
+var light_gray  = Color(194, 195, 199)
+var white       = Color(255, 241, 232)
+var red         = Color(255, 0, 77)
+var orange      = Color(255, 163, 0)
+var yellow      = Color(255, 236, 39)
+var green       = Color(0, 228, 54)
+var blue        = Color(41, 173, 255)
+var indigo      = Color(131, 118, 156)
+var pink        = Color(255, 119, 168)
+var peach       = Color(255, 204, 170)
