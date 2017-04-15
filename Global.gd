b95a3a8c78e9c825fbde9f6e468b33d6f2fa876a
Kamil Lewan <carmel4a@o2.pl>
2017-04-15 19:15:18 +0200

    Fix in global.gd
    - add height and width variable
    - fix in rand_arr()

--- Global.gd
+++ Global.gd
@@ -1,4 +1,9 @@
 extends Node
 
+var _width
+var _height
+
+func _ready():
+	_height = get_viewport().get_rect().size.y
+	_width = get_viewport().get_rect().size.x
 func rand_arr(arr):
-	return arr[randi() % arr.size()]
