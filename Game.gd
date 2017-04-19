d3dd86b8b27177eff442bf856b8da743e12b129e
Kamil Lewan <carmel4a@o2.pl>
2017-04-19 18:05:21 +0200

    Add level management

--- Game.gd
+++ Game.gd
@@ -7,18 +7,8 @@ onready var block = preload("res://Block/Block.tscn")
 
 func _ready():
 	randomize()
-	for x in range(columns):
-		blocks.append([])
-		for y in range(rows):
-			blocks[x].append([])
-			blocks[x][y] = set_block(x,y)
+	get_node("Level").set_script(load("res://Level/Random.gd"))
+	get_node("Level")._ready()
 
 func _on_ball_lost():
 	print("game over")
-
-func set_block(x,y):
-	blocks[x][y] = block.instance()
-	blocks[x][y].set_pos(Vector2(x*16,y*columns+3*16))
-	blocks[x][y].layer = y
-	blocks[x][y].type = randi() % 5
-	add_child(blocks[x][y],true)
---
+++ Level/Random.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var blocks = []
+var columns = 8
+var rows = 4
+onready var block = preload("res://Block/Block.tscn")
+
+func _ready():
+	for x in range(columns):
+			blocks.append([])
+			for y in range(rows):
+				blocks[x].append([])
+				blocks[x][y] = set_block(x,y, randi() % 5)
+func set_block(x,y,type):
+	if type != null:
+		blocks[x][y] = block.instance()
+		blocks[x][y].set_pos(Vector2(x*16,y*columns+3*16))
+		blocks[x][y].layer = y
+		blocks[x][y].type = type
+		add_child(blocks[x][y],true)
